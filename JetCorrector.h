939914ea6598d3d673c15da417d9de44314796df
Christopher A. George <george@physics.ucsb.edu>
2015-07-21 23:02:02 -0700

    fix class problem, add comment

--- JetCorrector.h
+++ JetCorrector.h
@@ -3,8 +3,11 @@
 #include <vector>
 #include <string>
 #include "TString.h"
+#include "jetcorr/FactorizedJetCorrector.h"
 
-class FactorizedJetCorrector;
+//Are you getting pissed off because of memory-related crashes?  
+//Don't forget that your vector of files has to be const!!  -Alex
+
 FactorizedJetCorrector *makeJetCorrector (const std::vector<std::string> &vector_of_file_names);
 
 #endif
